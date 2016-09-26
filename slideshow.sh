1510755ea8b5aac1e2d8b1a3cc08b969f7f633b6
lee2sman <leetusman@gmail.com>
2016-09-26 13:03:25 -0700

    added comment on feh

--- slideshow.sh
+++ slideshow.sh
@@ -8,5 +8,7 @@ fbi -noverbose -a -t $INTERVAL /home/pi/slides/*.jpg
 # here's almost exactly the same, but with photos appearing in random order
 # comment the above fbi line and uncomment this one to use it
 # fbi -noverbose -a -t $INTERVAL -u /home/pi/slides/*.jpg
-
+#
+# NEW alternate method using feh (thanks TechnologyClassroom for suggestion)
+# feh -FZYD 5 --cycle-once /home/pi/slides/*.png
 
